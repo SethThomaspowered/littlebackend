81cb1b3d5345b3cc593c3cb71aacbdfa77bc77f9
SethThomaspowered <athomas.seth@gmail.com>
2022-01-28 02:11:16 -0500

    trying to deploy to heroku

--- src/main/java/com/example/littlebackend/controller/BookController.java
+++ src/main/java/com/example/littlebackend/controller/BookController.java
@@ -11,6 +11,7 @@ import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(path="/api/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class BookController {
     private BookService bookService;
     private static final Logger LOGGER = Logger.getLogger(BookController.class.getName());
